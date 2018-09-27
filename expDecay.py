052c7d9cd7af107eff9109a9ddf3811f8c51fd38
Vedant Bhargava <s1612810@cplab174.ph.ed.ac.uk>
2018-09-27 11:13:32 +0100

    new file

--- expDecay.py
+++ expDecay.py
@@ -1,3 +1,28 @@
 import numpy as np
 
-class 
+class expdec(object):
+    def __init__(self, tau):
+        self.tau=tau
+
+    #returns random x with exponential distribution(one muon decay)
+    def muondec(self):
+        logic=True
+        while logic :
+            x=np.random.uniform(self.mean-10*self.width, self.mean+10*self.width)
+            y=np.random.uniform(0.0,1.0)
+            if y < self.evaluate(x):
+                logic = False
+        return x
+
+    #returns a numpy arrauy of 1000 muon decays
+    def kilomuon(self):
+        data=np.zeros(1000)
+        sum=0
+        for i in range (1000):
+            decay=self.muondec()
+            data[i]=decay
+            sum+=decay
+        self.kilodec=data
+        return sum/1000
+
+    
