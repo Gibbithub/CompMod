8b3c99906f764c9c00f9f0389acf16708508121e
Vedant Bhargava <s1612810@cplab177.ph.ed.ac.uk>
2018-10-11 14:02:09 +0100

    forgot to save

--- Checkpoint 2/Minimines.py
+++ Checkpoint 2/Minimines.py
@@ -2,8 +2,9 @@ import numpy as np
 from Chi import lineChi
 
 class Minimiser(object):
-    def __init__(self,params):
+    def __init__(self,function,params):
         self.param=params
-
-    def minimise(self,paramg,function)
-        
+        self.function=function
+    def minimise(self,paramg,function):
+        if (self.function(self.params) )
+        function(paramg)
