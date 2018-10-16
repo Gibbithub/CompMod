1cb6885ff566758967dc42f05a103048cb361149
Gibbithub <37941338+Gibbithub@users.noreply.github.com>
2018-10-16 22:59:29 +0100

    added c

--- Checkpoint 2/Chi.py
+++ Checkpoint 2/Chi.py
@@ -18,20 +18,23 @@ class lineChi(object):
             self.y.append(float(cols[1]))
             self.err.append(float(cols[2]))
 
+    #method to return value of chi- squared given a list of parameters
     def ChiEval(self,params):
         chi=0.
         for i in range (len(self.x)):
             chi += ((self.y[i]-(params[0]*self.x[i]+params[1]))/self.err[i])**2
         return chi
 
+    # methods to return chi-squared(as functions of m and c) with min dipped one below x axis for
     def ChiErrm(self,m):
-        chivalm=self.ChiEval(np.array([m,self.c]))-self.ChiEval(np.array([self.m,self.c]))-1
+        chivalm=self.ChiEval(np.array([m,self.c]))-self.ChiEval(np.array([self.m,self.c]))-1.
         return chivalm
 
     def ChiErrc(self,c):
-        chivalc=self.ChiEval(np.array([self.m,c]))-self.ChiEval(np.array([self.m,self.c]))-1
+        chivalc=self.ChiEval(np.array([self.m,c]))-self.ChiEval(np.array([self.m,self.c]))-1.
         return chivalc
 
+    #minimiser using scipy easier to implement within class to get lowest m and c vals
     def minimisesci(self,params):
         result=optimize.minimize(self.ChiEval,params)
         self.m,self.c=result.x
--- Checkpoint 2/FuncTest.py
+++ Checkpoint 2/FuncTest.py
@@ -6,24 +6,39 @@ import scipy.optimize as optimize
 import matplotlib.pyplot as plt
 
 def main():
-    dat="testData.txt"
+    dat="C:\Users\User\.atom\packages\Numerical Recipes\Checkpoint 2\\testData.txt"
     chi2=lineChi(dat)
     #myMinimiser=Minimiser(chi2.ChiEval,np.array([0.1,1.0]))
 
-    m,c= chi2.minimisesci(np.array([0.1,1.0]))
+    m,c= chi2.minimisesci(np.array([0.,0.0]))
     deltam= np.linspace((m-m),(m+m),100)
-    chivalm=np.zeros(100)
+    deltac= np.linspace((c-0.02*c),(c+0.02*c),100)
+    chivalm=np.zeros(100)#
+    chivalc=np.zeros(100)
     for i in range (100):
+        chivalc[i]=chi2.ChiErrc(deltac[i])
         chivalm[i]=chi2.ChiErrm(deltam[i])
     print m,c
-    errp=optimize.root(chi2.ChiErrm,np.array([0.4]))
-    print errp.x
-    errm=optimize.root(chi2.ChiErrm,np.array([-0.1]))
-    print errm.x
+    errmmax=optimize.root(chi2.ChiErrm,np.array([0.4]))
+    print errmmax.x
+    errmmin=optimize.root(chi2.ChiErrm,np.array([-0.1]))
+    print errmmin.x
+
+    errcmax=optimize.root(chi2.ChiErrc,np.array([1.000]))
+    print errcmax.x
+    errcmin=optimize.root(chi2.ChiErrc,np.array([0.990]))
+    print errcmin.x
 
     plt.plot(deltam,chivalm)
     plt.xlabel("m")
     plt.ylabel("chisquared - 1")
+    plt.axhline(y=0)
+    plt.show()
+
+    plt.plot(deltac,chivalc)
+    plt.xlabel("c")
+    plt.ylabel("chisquared - 1")
+    plt.axhline(y=0)
     plt.show()
 
 
